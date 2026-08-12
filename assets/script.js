3474d4b46cfb1325dd8cd74afed9be204a9ae042
Salomão Faleta <faletas41@gmail.com>
2026-08-12 16:46:57 -0300

    seção de competencias 1

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,30 @@
-let perfil = {
-    nome: "Salomão Faleta",
-    cargo: "Violista",
-    email: "📧 salomaooviola@gmail.com",
-    telefone: "📞 (71) 98660-1699",
-    cidade: "📍 Salvador-BA",
-    foto: "./assets/img/salomao.jpeg"
+let Sf = {
+    pessoal: {
+        nome: "Salomão Faleta",
+        cargo: "Violista",
+        email: "📧 salomaooviola@gmail.com",
+        telefone: "📞 (71) 98660-1699",
+        cidade: "📍 Salvador-BA",
+        foto: "./assets/img/salomao.jpeg"
+    },
+    formacao:{
+        curso: "Bacharelado em Música – Viola (em andamento)",
+        ufba: "Universidade Federal da Bahia • 2026 – 2030"
+    },
+    competencias:{
+        
+    }
 };
 
-document.getElementById("nome").innerHTML = perfil.nome;
-document.getElementById("cargo").innerHTML = perfil.cargo;
-document.getElementById("email").innerHTML = perfil.email;
-document.getElementById("telefone").innerHTML = perfil.telefone;
-document.getElementById("cidade").innerHTML = perfil.cidade
-document.getElementById("foto").src = perfil.foto;
+
+// HEADER
+document.getElementById("nome").innerHTML = Sf.pessoal.nome;
+document.getElementById("cargo").innerHTML = Sf.pessoal.cargo;
+document.getElementById("email").innerHTML = Sf.pessoal.email;
+document.getElementById("telefone").innerHTML = Sf.pessoal.telefone;
+document.getElementById("cidade").innerHTML = Sf.pessoal.cidade;
+document.getElementById("foto").src = Sf.pessoal.foto;
+
+// FORMAÇÃO
+document.getElementById("curso").innerHTML = Sf.formacao.curso;
+document.getElementById("ufba").innerHTML = Sf.formacao.ufba;
